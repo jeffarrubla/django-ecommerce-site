80520b7705ed8b311636575ce68ba578b8d7b039
Jefferson <jeffarrubla@gmail.com>
2020-04-08 21:17:29 +0000

    Rename model to singular

--- shop/models.py
+++ shop/models.py
@@ -2,7 +2,7 @@ from django.db import models
 
 # Create your models here.
 
-class Products(models.Model):
+class Product(models.Model):
 	title = models.CharField(max_length=200)
 	price = models.FloatField()
 	discount_price = models.FloatField()
