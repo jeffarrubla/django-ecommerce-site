490b6847b9ee0c3661674cb5983b64370b79de29
Jefferson <jeffarrubla@gmail.com>
2020-04-11 17:20:59 +0000

    Modifying action lists (admin side)

--- shop/admin.py
+++ shop/admin.py
@@ -6,9 +6,15 @@ admin.site.site_header = "E-commerce site"
 admin.site.site_title = "ABC Shopping"
 admin.site.index_title = "Manage ABC Shopping"
 
-class ProductAdmin(admin.ModelAdmin):
+class ProductAdmin(admin.ModelAdmin):	
 	list_display = ('title','price','discount_price','category','description','image')
 	search_fields = ('category',)
+	actions = ('change_category_to_default',) 
+
+	def change_category_to_default(self, request, queryset):
+		queryset.update(category='default')
+
+	change_category_to_default.short_description = 'Default Category'
 
 admin.site.register(Product,ProductAdmin)
 admin.site.register(Order)
