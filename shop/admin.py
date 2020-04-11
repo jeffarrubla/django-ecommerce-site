5be5ba9dee02f5b00c16abc43aad25056301af9d
Jefferson <jeffarrubla@gmail.com>
2020-04-11 16:42:01 +0000

    Updating headers (admin side)

--- shop/admin.py
+++ shop/admin.py
@@ -2,5 +2,9 @@ from django.contrib import admin
 from .models import Product, Order
 # Register your models here.
 
+admin.site.site_header = "E-commerce site"
+admin.site.site_title = "ABC Shopping"
+admin.site.index_title = "Manage ABC Shopping"
+
 admin.site.register(Product)
 admin.site.register(Order)
